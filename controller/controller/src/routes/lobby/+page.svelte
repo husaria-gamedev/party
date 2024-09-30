e8c44fdde8cdba02c9e5440a34bd1d553b0bcda2
OskarCzyz <49710785+OskarCzyz@users.noreply.github.com>
2024-09-30 23:35:08 +0200

    basic form logic

---
+++ controller/controller/src/routes/lobby/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { userStore } from '$lib/store';
+
+    type UserData = {
+        code: string;
+        name: string;
+    };
+
+    // Something isn't working here, needs a fix
+    let userData: UserData
+    userStore.subscribe((val) => userData = val)
+
+
+
+    
+</script>
+
+<div>
+    <h1>Lobby</h1>
+    {#if userData}
+        <h1>Welcome, {userData.name}!</h1>
+    {/if}
+</div>
