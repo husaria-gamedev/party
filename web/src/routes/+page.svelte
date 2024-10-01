e5e46c8878165757db9b2baefa0e8fcf364c9d15
Jan Sierpina <jansierpina@gmail.com>
2024-10-01 22:30:02 +0200

    basic login screen

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="grid place-items-center h-screen bg-green-100">
+    <div class="card bg-base-100 w-96 shadow-xl">
+    <figure>
+        <img
+        src="party.webp"
+        alt="Shoes" />
+    </figure>
+    <div class="card-body">
+        <h2 class="card-title">Your name</h2>
+        <label class="input input-bordered flex items-center gap-2">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            class="h-4 w-4 opacity-70">
+            <path
+            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+        </svg>
+        <input type="text" class="grow" placeholder="Username" />
+        </label>
+
+        <div class="grid place-items-center card-actions">
+            <button class="btn">Connect</button>
+        </div>
+    </div>
+    </div>
+</div>
